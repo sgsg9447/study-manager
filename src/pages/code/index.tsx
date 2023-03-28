e36ee4ff4ed6b5e2275ce25462fd99c5d4fdafb2
sgsg9447 <sgsg9447@gmail.com>
2023-03-28 22:54:36 +0900

    feat : code page, Modal component, Text component UI publishing

--- src/pages/code/index.tsx
+++ src/pages/code/index.tsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+import Button from "../../components/common/Button";
+import Modal from "../../components/common/Modal";
+
 export default function Code() {
-  return <div>Code</div>;
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const clickModal = () => {
+    setIsOpenModal(true);
+  };
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submit!!");
+    //TODO : 콜백함수로 Modal에서 받은 input 값 가져오기
+  };
+  return (
+    <div className="container">
+      <div className="code">
+        <Button onClick={clickModal} style={{ width: "300px" }}>
+          입장코드
+        </Button>
+        {isOpenModal && (
+          <Modal variant="code" onSubmit={onSubmit} closeModal={closeModal} />
+        )}
+      </div>
+    </div>
+  );
 }
