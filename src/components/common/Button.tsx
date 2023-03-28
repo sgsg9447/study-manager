6efa6b0277a78abd9cc87c3aeac0294b049b8db8
sgsg9447 <sgsg9447@gmail.com>
2023-03-28 20:10:52 +0900

    feat : home page, button component ui publishing

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,38 @@
+import { CSSProperties, useMemo } from "react";
+
+type Variant = "outlined" | "contained" | "text";
+
+export default function Button({
+  children,
+  onClick,
+  variant = "contained",
+  size = "md",
+  style,
+}: {
+  children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  variant?: Variant;
+  size?: Size;
+  style?: CSSProperties;
+}) {
+  const getVariantStyle = (variant: Variant) => {
+    const styleObj = {
+      outlined: "btn-outlined",
+      contained: "btn-primary",
+      text: "btn-text",
+    };
+    const styleMap = new Map(Object.entries(styleObj));
+    return styleMap.get(variant);
+  };
+  const getSizeStyle = (size: Size) => `btn-${size}`;
+  const _style = useMemo(() => getVariantStyle(variant), [variant]);
+  return (
+    <button
+      className={`${getVariantStyle(variant)} ${getSizeStyle(size)}`}
+      onClick={onClick}
+      style={style}
+    >
+      {children}
+    </button>
+  );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,13 @@
+import Button from "../../components/common/Button";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="home-wrapper">
+        <Button onClick={() => console.log("click")} style={{ width: "300px" }}>
+          로그인하기
+        </Button>
+      </div>
+    </div>
+  );
 }
